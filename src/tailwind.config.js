08cb907bf71a03749352cf12a61e4897b8e872c3
sonny-xxiv <oludelesamson3@gmail.com>
2025-11-25 14:59:07 +0100

    Topp Farm commit

---
+++ src/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    theme: {
+      extend: {
+        fontFamily: {
+          cover: ["Covered By Your Grace", 'cursive'],
+          man: ["Manrope", 'sans-serif'],
+          poppins: ["Poppins", 'sans-serif']
+        },
+      },
+    },
+  }
+  
